4bedfdf33dcaa946cef01240c0b61a6a96c7d260
Thomas Debarochez <thomas.barochez+github@gmail.com>
2011-04-07 23:12:40 +0200

    modify example for more efficiency

--- example.js
+++ example.js
@@ -2,17 +2,26 @@ var cache = require('./lib/connect-cache');
 var connect = require('connect');
 
 function helloWorld(req, res) {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
   if (req.url == '/') {
-    res.write('no cached result');
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.write('direct result');
+    res.end('');
   }
-  if (req.url == '/path') {
-    res.write('cached result');
+  else if (req.url == '/path') {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    setTimeout(function () {
+      res.write('cached result');
+      res.end('');
+    }, 1000);
   }
-  res.end('');
+  else {
+    res.writeHead(404, { 'Content-Type': 'text/plain' });
+    res.write('not found');
+    res.end('');
+  }
 }
-var server = connect.createServer(cache({regex: /path/,
-                                         ttl: 60}),
+var server = connect.createServer(cache({regex: /path.*/,
+                                         ttl: 60000}),
                                   helloWorld);
 
 server.listen(3000);
