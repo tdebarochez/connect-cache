e18416e4625fdb6a0d6d6c7f5c90acc93d62241c
Thomas Debarochez <thomas.barochez+github@gmail.com>
2011-04-13 13:13:46 +0200

    http.Server.listen() is asynchronous

--- test/connect-cache.test.js
+++ test/connect-cache.test.js
@@ -4,7 +4,7 @@ var cache = require('../lib/connect-cache')
   , fs = require('fs')
   , assert = require('assert');
 
-function run_server(port) {
+function run_server(port, cb) {
   var server = connect.createServer(cache({regex: /path.*|test.jpg/,
                                            loopback: 'localhost:' + port,
                                            ttl: 60000}),
@@ -30,104 +30,109 @@ function run_server(port) {
                                         res.end('not found');
                                       }
                                     });
-  server.listen(port);
+  server.listen(port, function () {
+    cb(server);
+  });
   return server;
 }
 
 module.exports = {
   "direct result": function () {
-    var server = run_server(3462);
-    var options = {
-      host: 'localhost',
-      port: 3462,
-      path: '/'
-    };
-    http.get(options, function (response) {
-      assert.strictEqual(response.statusCode, 200);
-      var body = '';
-      response.on('data', function (chunk) {
-        body += chunk;
-      });
-      response.on('end', function () {
-        assert.equal(body, 'direct result');
-        server.close();
+    run_server(3462, function (server) {
+      var options = {
+        host: 'localhost',
+        port: 3462,
+        path: '/'
+      };
+      http.get(options, function (response) {
+        assert.strictEqual(response.statusCode, 200);
+        var body = '';
+        response.on('data', function (chunk) {
+          body += chunk;
+        });
+        response.on('end', function () {
+          assert.equal(body, 'direct result');
+          server.close();
+        });
       });
     });
   },
   "cached result": function () {
-    var server = run_server(3463);
-    var options = {
-      host: 'localhost',
-      port: 3463,
-      path: '/path'
-    };
-    var start = +new Date;
-    http.get(options, function (response) {
-      assert.strictEqual(response.statusCode, 200);
-      var body = '';
-      response.on('data', function (chunk) {
-        body += chunk;
-      });
-      response.on('end', function () {
-        assert.strictEqual(+new Date - start >= 500, true);
-        assert.strictEqual(body, 'cached result');
-        start = +new Date;
-        http.get(options, function (response) {
-          assert.strictEqual(response.statusCode, 200);
-          var body = '';
-          response.on('data', function (chunk) {
-            body += chunk;
-          });
-          response.on('end', function () {
-            assert.strictEqual(+new Date - start <= 500, true);
-            assert.strictEqual(body, 'cached result');
-            server.close();
+    run_server(3463, function (server) {
+      var options = {
+        host: 'localhost',
+        port: 3463,
+        path: '/path'
+      };
+      var start = +new Date;
+      http.get(options, function (response) {
+        assert.strictEqual(response.statusCode, 200);
+        var body = '';
+        response.on('data', function (chunk) {
+          body += chunk;
+        });
+        response.on('end', function () {
+          assert.strictEqual(+new Date - start >= 500, true);
+          assert.strictEqual(body, 'cached result');
+          start = +new Date;
+          http.get(options, function (response) {
+            assert.strictEqual(response.statusCode, 200);
+            var body = '';
+            response.on('data', function (chunk) {
+              body += chunk;
+            });
+            response.on('end', function () {
+              assert.strictEqual(+new Date - start <= 500, true);
+              assert.strictEqual(body, 'cached result');
+              server.close();
+            });
           });
         });
       });
     });
   },
   "binary result": function () {
-    var server = run_server(3464);
-    var options = {
-      host: 'localhost',
-      port: 3464,
-      path: '/test.jpg'
-    };
-    http.get(options, function (response) {
-      assert.strictEqual(response.statusCode, 200);
-      var chunks = []
-        , length = 0;
-      response.on('data', function (chunk) {
-        chunks.push(new Buffer(chunk, 'binary'));
-        length += chunk.length;
-      });
-      response.on('end', function () {
-        var content = new Buffer(length);
-        var offset = 0;
-        chunks.forEach(function (chunk) {
-          chunk.copy(content, offset);
-          offset += chunk.length;
+    var server = run_server(3464, function (server) {
+      var options = {
+        host: 'localhost',
+        port: 3464,
+        path: '/test.jpg'
+      };
+      http.get(options, function (response) {
+        assert.strictEqual(response.statusCode, 200);
+        var chunks = []
+          , length = 0;
+        response.on('data', function (chunk) {
+          chunks.push(new Buffer(chunk, 'binary'));
+          length += chunk.length;
         });
-        assert.strictEqual(length, fs.readFileSync('./static/test.jpg', 'binary').length);
-        fs.renameSync('./static/test.jpg', 'static/test.jpg_');
-        http.get(options, function (response) {
-          assert.strictEqual(response.statusCode, 200);
-          var chunks = [];
-          response.on('data', function (chunk) {
-            chunks.push(new Buffer(chunk, 'binary'));
-            length += chunk.length;
+        response.on('end', function () {
+          var content = new Buffer(length);
+          var offset = 0;
+          chunks.forEach(function (chunk) {
+            chunk.copy(content, offset);
+            offset += chunk.length;
           });
-          response.on('end', function () {
-            server.close();
-            var content = new Buffer(length);
-            var offset = 0;
-            chunks.forEach(function (chunk) {
-              chunk.copy(content, offset);
-              offset += chunk.length;
+          assert.strictEqual(length, fs.readFileSync('./static/test.jpg', 'binary').length);
+          fs.renameSync('./static/test.jpg', 'static/test.jpg_');
+          http.get(options, function (response) {
+            assert.strictEqual(response.statusCode, 200);
+            var chunks = [];
+            response.on('data', function (chunk) {
+              chunks.push(new Buffer(chunk, 'binary'));
+              length += chunk.length;
             });
-            assert.strictEqual(length, fs.readFileSync('./static/test.jpg_', 'binary').length);
-            fs.renameSync('./static/test.jpg_', 'static/test.jpg');
+            response.on('end', function () {
+              server.close();
+              var content = new Buffer(length);
+              var offset = 0;
+              chunks.forEach(function (chunk) {
+                chunk.copy(content, offset);
+                offset += chunk.length;
+              });
+              assert.strictEqual(length, fs.readFileSync('./static/test.jpg_', 'binary').length);
+              fs.renameSync('./static/test.jpg_', 'static/test.jpg');
+            });
           });
         });
       });
